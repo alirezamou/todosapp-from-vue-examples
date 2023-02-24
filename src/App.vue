9717de2854ebea66acf2d8cd33e11ed4c5fbbf8f
alirezamou <am808036@gmail.com>
2023-02-24 20:20:05 +0330

    build structure of page and style from exmple

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,40 @@
 </script>
 
 <template>
+  <section class="todoapp">
+    <header class="header">
+      <h1>todos</h1>
+      <input type="text" class="new-todo">
+    </header>
+    <section class="main">
+      <input type="text" class="toggle-all">
+      <label for="toggle-all">Mark all as completed</label>
+      <ul class="todo-list">
+        <li class="todo">
+          <div class="view">
+            <input type="text" class="toggle">
+            <label for="toggle"></label>
+            <button class="destroy"></button>
+          </div>
+          <input type="text" class="edit">
+        </li>
+      </ul>
+    </section>
+    <footer class="footer">
+      <span class="todo-count">
+        <strong></strong>
+        <span></span>
+      </span>
+      <ul class="filters">
+        <li><a href="#/all" :class="{ selected: visibility === 'all' }">All</a></li>
+        <li><a href="#/active" :class="{ selected: visibility === 'active'}" >Active</a></li>
+        <li><a href="#/completed" :class="{ selected: visibility === 'completed' }">Completed</a></li>
+      </ul>
+      <button class="clear-completed">Clear completed</button>
+    </footer>
+  </section>
 </template>
 
-<style scoped>
+<style>
+@import "https://unpkg.com/todomvc-app-css@2.4.1/index.css";
 </style>
