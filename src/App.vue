0a96159d24795086dcf5458aadb6883d24d0744a
alirezamou <am808036@gmail.com>
2023-02-24 23:17:22 +0330

    fix - hidden clear completed button if there is not any todo completed

--- src/App.vue
+++ src/App.vue
@@ -84,7 +84,7 @@ export default {
         <li><a href="#active" :class="{ selected: visibility === 'active'}" >Active</a></li>
         <li><a href="#completed" :class="{ selected: visibility === 'completed' }">Completed</a></li>
       </ul>
-      <button class="clear-completed" @click="clearCompleted">Clear completed</button>
+      <button class="clear-completed" @click="clearCompleted" v-if="todos.length > remaining">Clear completed</button>
     </footer>
   </section>
 </template>
